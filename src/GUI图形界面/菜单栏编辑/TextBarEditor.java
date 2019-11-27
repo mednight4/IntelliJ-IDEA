68f660c6c941f04dfc5e4caaa7737f544f5219a2
flower fire <mednight4@github.com>
2019-11-27 08:31:09 +0800

    同步

--- src/GUI图形界面/菜单栏编辑/TextBarEditor.java
+++ src/GUI图形界面/菜单栏编辑/TextBarEditor.java
@@ -1,25 +1,27 @@
-package GUI图形界面.菜单栏;
-import com.sun.source.tree.NewArrayTree;
+package GUI图形界面.菜单栏编辑;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.FileWriter;
 import java.io.FileReader;
-import java.nio.channels.ClosedChannelException;
 import javax.swing.JFileChooser;
-public class TextEditor extends Frame implements ActionListener{
+public class TextBarEditor extends Frame implements ActionListener{
     MenuBar mainmenubar = new MenuBar();//申明菜单条
     Menu file ; //申明主菜单项
+    Menu editor;//申明编辑菜单
+    MenuItem cp;
+    MenuItem ct;
+    MenuItem pa;
     MenuItem nw;//申明各子菜单项
     MenuItem op;
     MenuItem cl;
     MenuItem sf;
     MenuItem ex;
     TextArea tx;//申明文本区对象
-    public TextEditor(String title){
+    public TextBarEditor(String title){
         super (title);//调用父类构造方法
-        CloseHandler handler=new CloseHandler();//定义窗体事件的侦听器对象
+        GUI图形界面.菜单栏编辑.CloseHandler handler=new GUI图形界面.菜单栏编辑.CloseHandler();//定义窗体事件的侦听器对象
         this.addWindowListener(handler); //为当前窗体注册侦听器对象
-        setSize (400,400);//设置窗体尺寸
+        setSize (800,800);//设置窗体尺寸
         setLocationRelativeTo(null);//使窗体在屏幕上居中显示
         menuinit();//构建与处理菜单
         tx=new TextArea();//创建文本区对象
@@ -30,16 +32,24 @@ public class TextEditor extends Frame implements ActionListener{
     void menuinit() {
         mainmenubar = new MenuBar();//定义主菜单栏
         file = new Menu("文件");
-        nw = new MenuItem("新建文件");
-        op = new MenuItem("打开文件");
-        cl = new MenuItem("关闭文件");
-        sf = new MenuItem("保存文件");
-        ex = new MenuItem("退   出");
+        editor=new Menu("编辑");
+        cp=new MenuItem("编辑");
+        ct=new MenuItem("剪贴");
+        pa=new MenuItem("复制");
+        nw = new MenuItem("新建");
+        op = new MenuItem("打开");
+        cl = new MenuItem("关闭");
+        sf = new MenuItem("保存");
+        ex = new MenuItem("退出");
+        editor.add(pa);//将各子菜单项加入到主菜单项中
+        editor.add(cp);
+        editor.add(ct);
         file.add(nw);//将各子菜单项加入到主菜单项中
         file.add(op);
         file.add(cl);
         file.add(sf);
         file.add(ex);
+        mainmenubar.add(editor);
         mainmenubar.add(file);//将主菜单项加入到主菜单栏中
         setMenuBar(mainmenubar);//为窗体设置主菜单
         nw.addActionListener(this);//为各菜单项注册事件侦听器
@@ -47,11 +57,15 @@ public class TextEditor extends Frame implements ActionListener{
         cl.addActionListener(this);//为各菜单项注册事件侦听器
         sf.addActionListener(this);//为各菜单项注册事件侦听器
         ex.addActionListener(this);//为各菜单项注册事件侦听器
+        cp.addActionListener(this);//为各菜单项注册事件侦听器
+        pa.addActionListener(this);//为各菜单项注册事件侦听器
+        ct.addActionListener(this);//为各菜单项注册事件侦听器
     }
-//窗体的ActionEvent事件处理方法
+    //窗体的ActionEvent事件处理方法
     public void actionPerformed(ActionEvent e){
         Object ob=e.getSource();//获取事件对象
         JFileChooser f=new JFileChooser();//创建文件选择器对象
+        String copy="";
         if ((ob==nw)||(ob==cl)){
             //选择"新建文件"或者"关闭文件"子菜单项
             tx.setText("");//清空文本区
@@ -71,12 +85,12 @@ public class TextEditor extends Frame implements ActionListener{
                     if (b==-1)
                         break;
                     s.append((char)b);
-                    }
-                    tx.setText(s.toString());//将文件内天显示在文本区
-                    in.close();//关闭文件
-                }catch(Exception ee){
                 }
-            }else if(ob==sf){//选择"保存文件"子菜单项
+                tx.setText(s.toString());//将文件内天显示在文本区
+                in.close();//关闭文件
+            }catch(Exception ee){
+            }
+        }else if(ob==sf){//选择"保存文件"子菜单项
             f.showSaveDialog(this);
             try{
                 //创建FileWriter对象，其参数为前面选择的文件
@@ -85,13 +99,23 @@ public class TextEditor extends Frame implements ActionListener{
                 out.close();//关闭文件
             }catch (Exception ee){
             }
-        }else if(ob==ex)
+        }else if(ob==ex) {
             System.exit(0);
         }
+        else if (ob==cp){
+            copy=tx.getText();
+        }
+        else if (ob==ct){
+            copy=tx.getText();
+            tx.setText("");//清空文本区
+        }else if(ob==pa){
+            tx.setText(copy);
+        }
+    }
 
 
     public static void main(String[] args){
-        new TextEditor("简易文本编辑器");
+        new GUI图形界面.菜单栏编辑.TextBarEditor("简易文本编辑器");
     }
 }
 //CloseHandler类实现关闭文件窗口的功能
@@ -102,5 +126,4 @@ class CloseHandler extends WindowAdapter{
         System.exit(0);//终止当前线程
     }
 }
-
 
